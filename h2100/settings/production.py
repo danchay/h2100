1374f406a1ddbe6bb6190f305b13e0b36d47fa48
Dan Chay <danchay@gmail.com>
2017-01-30 14:43:41 -0900

    Changed project_root path

--- h2100/settings/production.py
+++ h2100/settings/production.py
@@ -139,7 +139,7 @@ SHORTCODE_MAX = 16
 SHORTCODE_MIN = 4
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
