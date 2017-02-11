7cd94bad48b1d31d931a78a88fafb9e8e038c24a
Dan Chay <danchay@gmail.com>
2017-02-10 15:32:38 -0900

    Added SECRET_KEY to os.environ[]

--- h2100/settings/production.py
+++ h2100/settings/production.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1!3)52_eu&!5k*2j7z3e@ay2i9v=l0_qud2c29&@&zoeh!yqsv'
-
+# SECRET_KEY = ''
+SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
