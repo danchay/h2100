f8bdf47c67e14896876c9ed48eb7957205fc6ca2
Dan Chay <danchay@gmail.com>
2017-02-10 15:01:12 -0900

    Adjusted MEDIA_ROOT

--- h2100/settings/production.py
+++ h2100/settings/production.py
@@ -153,6 +153,7 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
--- h2100/urls.py
+++ h2100/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from .settings import MEDIA_URL, MEDIA_ROOT
+from .settings import MEDIA_URL
 from .settings import STATIC_URL, STATIC_ROOT
 
 
@@ -31,5 +31,6 @@ urlpatterns = [
 ] 
 
 if settings.DEBUG:
+    from .settings import MEDIA_ROOT
     urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
     urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
