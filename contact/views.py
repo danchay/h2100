9f37dbaddec0ad282c99894eef1ef51a4e110dc7
Dan Chay <danchay@gmail.com>
2017-02-12 17:07:05 -0900

    Adjusted local variable in contact view. Changed form in index.html

--- contact/views.py
+++ contact/views.py
@@ -15,26 +15,36 @@ from django.core.mail import send_mail
 def contact(request):
 	title = "Send Us a Message"
 	form = ContactForm(request.POST or None)
+	form_email = ''
+	form_full_name = ''
+	form_message = ''
+	context = {
+		'title': title,
+		'form': form
+		}
 	if form.is_valid():
 		form_email = form.cleaned_data.get("email")
 		form_full_name = form.cleaned_data.get("full_name")
 		form_message = form.cleaned_data.get("message")
-		# print(form.cleaned_data)
+		# print(form_email)
 	subject = 'H2100 contact form'
 	from_email = settings.EMAIL_HOST_USER 
 	to_email = [from_email, 'danchay@gmail.com']
-	contact_message = "%s: %s via %s"%(form_full_name, form_message, form_email)
+	contact_message = "%s via %s -- Message: %s" % (
+		form_full_name,
+		form_email, 
+		form_message 
+		)
 
 	send_mail(subject, 
 		contact_message, 
 		from_email, 
 		to_email, 
 		fail_silently=False)
+	context = {
+		'title': "Message sent. Thank you."
+	}
 
-	context={
-		'title': title,
-		'form': form
-		}
 
 	return render(request, "contact/index.html", context)
 
