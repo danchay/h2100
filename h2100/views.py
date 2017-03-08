9afeeabe7858dd48459931ef65efca33bf8f861c
Dan Chay <danchay@gmail.com>
2017-03-08 09:43:52 -0900

    Date again

--- h2100/views.py
+++ h2100/views.py
@@ -13,51 +13,52 @@ from blog.models import Post
 
 
 def about(request):
-	today = date.today()
-	mn_list_str_1 = str(today.year) + '/' + str(today.month) + '/' + str(today.day)
-	mn_list_str_2 = str(today.year + 1) + '/' + str(today.month) + '/' + str(today.day)
-	future = date(2058,2,16)
-	days = (future - today).days
-	# days = '{:.{prec}f}'.format(days, prec=1)
-	days = '{:,}'.format(days)
-	weeks = round(((future-today).days/7),1)
-	# weeks = '{:.{prec}f}'.format(weeks, prec=1)
-	weeks = '{:,}'.format(weeks)
-	months = round(((future-today).days/30.44),1)
-	# months = '{:.{prec}f}'.format(months, prec=1)
-	months = '{:,}'.format(months)
-	lunar_months = round(((future-today).days/29.53),1)
-	years = round(((future-today).days/365.25),1)
-	#years = '{:.{prec}f}'.format(years, prec=1)
-	mars_years = round(((future-today).days/668.5991),1)
-	# mars_years = '{:.{prec}f}'.format(mars_years, prec=1)
-	moon_list = fm_series( mn_list_str_1, mn_list_str_2)
-	uc_married_dt = '1987/9/18'
-	offcly_married_dt = '1988/8/27'
-	fm_since_uc_married = flmoons_since(uc_married_dt, mn_list_str_2)[0]
-	fm_since_of_married = flmoons_since(offcly_married_dt, mn_list_str_2)[0]
-	bm_since_of_married = flmoons_since(offcly_married_dt, mn_list_str_2)[1]
+    today = date.today()
+    mn_list_str_1 = str(today.year) + '/' + str(today.month) + '/' + str(today.day)
+    mn_list_str_2 = str(today.year + 1) + '/' + str(today.month) + '/' + str(today.day)
+    future = date(2058,2,16)
+    days = (future - today).days
+    # days = '{:.{prec}f}'.format(days, prec=1)
+    days = '{:,}'.format(days)
+    weeks = round(((future-today).days/7),1)
+    # weeks = '{:.{prec}f}'.format(weeks, prec=1)
+    weeks = '{:,}'.format(weeks)
+    months = round(((future-today).days/30.44),1)
+    # months = '{:.{prec}f}'.format(months, prec=1)
+    months = '{:,}'.format(months)
+    lunar_months = round(((future-today).days/29.53),1)
+    years = round(((future-today).days/365.25),1)
+    #years = '{:.{prec}f}'.format(years, prec=1)
+    mars_years = round(((future-today).days/668.5991),1)
+    # mars_years = '{:.{prec}f}'.format(mars_years, prec=1)
+    moon_list = fm_series( mn_list_str_1, mn_list_str_2)
+    uc_married_dt = '1987/9/18'
+    offcly_married_dt = '1988/8/27'
+    fm_since_uc_married = flmoons_since(uc_married_dt, mn_list_str_2)[0]
+    fm_since_of_married = flmoons_since(offcly_married_dt, mn_list_str_2)[0]
+    bm_since_of_married = flmoons_since(offcly_married_dt, mn_list_str_2)[1]
 
 
-	context = {
-		"days": days,
-		"weeks": weeks,
-		"months": months,
-		"lunar_months": lunar_months,
-		"years": years,
-		"mars_years": mars_years,
-		"moon_list": moon_list,
-		"fm_since_uc_married": fm_since_uc_married,
-		"fm_since_of_married": fm_since_of_married,
-		"bm_since_of_married": bm_since_of_married,
-		}
-	return render(request, "about.html", context )
+    context = {
+        "days": days,
+        "weeks": weeks,
+        "months": months,
+        "lunar_months": lunar_months,
+        "years": years,
+        "mars_years": mars_years,
+        "moon_list": moon_list,
+        "fm_since_uc_married": fm_since_uc_married,
+        "fm_since_of_married": fm_since_of_married,
+        "bm_since_of_married": bm_since_of_married,
+        }
+    return render(request, "about.html", context )
 
 def get_popular_posts():
     popular_posts = Post.objects.order_by('-views')[:5]
     return popular_posts
 
 def index(request):
+    date = ''
     latest_posts = Post.objects.all().order_by('-created_date')[:10]
 
 
@@ -68,8 +69,6 @@ def index(request):
     paginator = Paginator(latest_posts, 8)
     queryset_list = Post.objects.all().order_by('-created_date')
 
-    for post in queryset_list:
-    	date = post.created_date.strftime('%a, %d %b %Y')   
 
     query = request.GET.get("q")
     if query:
