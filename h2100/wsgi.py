7b79a82cff771be713ce7fa8a0259ae10d0e8a69
Dan Chay <danchay@gmail.com>
2017-03-22 14:31:30 -0800

    wsgi

--- h2100/wsgi.py
+++ h2100/wsgi.py
@@ -18,7 +18,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h2100.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h2100.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
