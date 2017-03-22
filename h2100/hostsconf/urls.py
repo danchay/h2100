19f658a512dfaff5acb4237dee9addf511f5476e
Dan Chay <danchay@gmail.com>
2017-03-22 14:14:22 -0800

    Cleanup

--- h2100/hostsconf/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from .views import wildcard_redirect
-
-urlpatterns = [
-    url(r'^(?P<path>.*)', wildcard_redirect),
-
-]
