c85eae72a9cb30cb2fd7f960c69a7f4c2f6aecfd
Dan Chay <danchay@gmail.com>
2017-03-22 13:41:57 -0800

    Settings change

--- h2100/settings/__init__.py
+++ h2100/settings/__init__.py
@@ -1,12 +1,11 @@
 try:
+	from .pglocal import *
+except:
+	try:
+		from .sqlite import *
+	except:
+		pass
+try:
 	from .production import *
 except:
 	pass
-
-
-
-try: 
-	from .sqlite import *
-except: 
-	pass
-
