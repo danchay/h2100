8f638de77224cdd4fbb1027d791ddea8f982ce6b
Dan Chay <danchay@gmail.com>
2017-04-04 21:41:14 -0800

    Refactored index view to use test template use

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from blog.views import index, healthspan, training, eating, sleeping, learning, other, post
 
@@ -9,19 +10,23 @@ class IndexPageTest(TestCase):
 
 	# Resolve / URL to index view? Import resolve
 	# Make view function return HTML import blog.views index
-	def test_root_url_resolves_to_index_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, index)
+	# def test_root_url_resolves_to_index_view(self):
+	# 	found = resolve('/')
+	# 	self.assertEqual(found.func, index)
 
 	# Import HttpRequest
 	def test_index_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = index(request)
+		# request = HttpRequest()
+		# response = index(request)
+		response = self.client.get('/')
+
 		html = response.content.decode('utf8')
 		self.assertIn('<html lang="en">', html)
 		self.assertIn('<title>H2100</title>', html)
 		self.assertIn('favicon-32x32.png', html)
 		self.assertIn('</html>', html)
+		self.assertTrue(html.strip().endswith('</html>'))
+		self.assertTemplateUsed(response, 'blog/index.html')
 
 	def test_index_page_displays_ckwote_in_html(self):
 		request = HttpRequest()
