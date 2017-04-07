1fa5c3dbebcd7fe68c343b5a3ca0865c7ec5fa16
Dan Chay <danchay@gmail.com>
2017-04-06 16:28:32 -0800

    Functional test work

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,25 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
+
+def wait_for_execution(test_obj):
+	start_time = time.time()
+	while True:
+		try:
+			test_obj
+			return 
+		except (AssertionError, WebDriverException) as e:
+			if time.time() -start_time > MAX_WAIT:
+				raise e 
+			time.sleep(0.5)
+
+
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -12,72 +28,90 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_page_elements_render(self):
-		# She opens homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
-		# She notices the page title
 		self.assertIn('H2100', self.browser.title)
-		
-		# She notices the logo image
+
+	def test_logo_render(self):
 		logo = self.browser.find_element_by_css_selector("img[src$='favicon-32x32.png'].navbar-brand")
 		self.assertTrue(logo)
 
-		# She notices 'Hacking to 100'
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Hacking to 100', header_text)
 
-		# She notices '...xxxx days pending.' in the nav bar
-		# The string will be longer than 18 elements long if digits in number days > 2
-		days_pending = self.browser.find_element_by_xpath("//div[@class='navbar-header']/span[contains(text(), 'days pending.')]")
-		self.assertTrue(len(days_pending.text) > 18)
+# class NewVisitorTest(unittest.TestCase):
 
-		# She notices a quotation at the top of the front page
-		ckwote = self.browser.find_element_by_tag_name('em').text 
-		self.assertTrue(ckwote.startswith('"'))
-		self.assertIn('"', ckwote)
-		self.assertTrue(len(ckwote) > 10)
+# 	def setUp(self):
+# 		self.browser = webdriver.Firefox()
 
-		# She notices 'Most Popular Posts' w/ 5 links
-		mp_posts = self.browser.find_element_by_tag_name('h4').text 
-		self.assertIn('Most Popular Posts', mp_posts)
+# 	def tearDown(self):
+# 		self.browser.quit()
 
-		# She notices the Dan Chay email address
-		email = self.browser.find_element_by_tag_name('address').text 
-		self.assertIn('Dan Chay', email)
+# 	def test_page_elements_render(self):
+# 		# She opens homepage
+# 		self.browser.get('http://localhost:8000')
 
-		# She sees that there is a list of post items
-		posts = self.browser.find_elements_by_class_name('post-item')
-		self.assertTrue(len(posts) >= 1)
-		self.assertFalse(len(posts) >= 5)
+# 		# She notices the page title
+# 		self.assertIn('H2100', self.browser.title)
+		
+# 		# She notices the logo image
+# 		logo = self.browser.find_element_by_css_selector("img[src$='favicon-32x32.png'].navbar-brand")
+# 		self.assertTrue(logo)
 
-		# She sees 'Page x of x. Next'
-		pages = self.browser.find_element_by_class_name('current').text
-		self.assertIn('Page', pages)
+# 		# She notices 'Hacking to 100'
+# 		header_text = self.browser.find_element_by_tag_name('h1').text
+# 		self.assertIn('Hacking to 100', header_text)
 
-		# She is invited to search titles and tags in a search form
-		search_input_box = self.browser.find_element_by_class_name('form-control')
-		self.assertEqual(search_input_box.get_attribute('placeholder'), 'Search in titles and tags')
+# 		# She notices '...xxxx days pending.' in the nav bar
+# 		# The string will be longer than 18 elements long if digits in number days > 2
+# 		days_pending = self.browser.find_element_by_xpath("//div[@class='navbar-header']/span[contains(text(), 'days pending.')]")
+# 		self.assertTrue(len(days_pending.text) > 18)
 
-		# She does a search on the first listed post
-		search_item = self.browser.find_element_by_xpath("//div[@class='col-md-8']/ul/h2/a")
-		self.assertTrue(search_item)
-		self.assertTrue(search_item.text)
-		search_item=search_item.text
-		search_input_box.send_keys(search_item)
-		search_input_box.send_keys(Keys.ENTER)
-		time.sleep(1)
-		new_header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Search Results', new_header_text)
-		self.assertTrue(search_item)
+# 		# She notices a quotation at the top of the front page
+# 		ckwote = self.browser.find_element_by_tag_name('em').text 
+# 		self.assertTrue(ckwote.startswith('"'))
+# 		self.assertIn('"', ckwote)
+# 		self.assertTrue(len(ckwote) > 10)
 
-		# She clicks on a post title and it returns a single post with Tag:
-		post_title = search_item
-		post_title_link = self.browser.find_element_by_link_text(post_title)
-		post_title_link.click()
-		time.sleep(1)
+# 		# She notices 'Most Popular Posts' w/ 5 links
+# 		mp_posts = self.browser.find_element_by_tag_name('h4').text 
+# 		self.assertIn('Most Popular Posts', mp_posts)
 
-		tag = self.browser.find_element_by_css_selector("p.pull-right").text
-		self.assertIn('Tag:', tag)
+# 		# She notices the Dan Chay email address
+# 		email = self.browser.find_element_by_tag_name('address').text 
+# 		self.assertIn('Dan Chay', email)
+
+# 		# She sees that there is a list of post items
+# 		posts = self.browser.find_elements_by_class_name('post-item')
+# 		self.assertTrue(len(posts) >= 1)
+# 		self.assertFalse(len(posts) >= 5)
+
+# 		# She sees 'Page x of x. Next'
+# 		pages = self.browser.find_element_by_class_name('current').text
+# 		self.assertIn('Page', pages)
+
+# 		# She is invited to search titles and tags in a search form
+# 		search_input_box = self.browser.find_element_by_class_name('form-control')
+# 		self.assertEqual(search_input_box.get_attribute('placeholder'), 'Search in titles and tags')
+
+# 		# She does a search on the first listed post
+# 		search_item = self.browser.find_element_by_xpath("//div[@class='col-md-8']/ul/h2/a")
+# 		self.assertTrue(search_item)
+# 		self.assertTrue(search_item.text)
+# 		search_item=search_item.text
+# 		search_input_box.send_keys(search_item)
+# 		search_input_box.send_keys(Keys.ENTER)
+# 		time.sleep(1)
+# 		new_header_text = self.browser.find_element_by_tag_name('h1').text
+# 		self.assertIn('Search Results', new_header_text)
+# 		self.assertTrue(search_item)
+
+# 		# She clicks on a post title and it returns a single post with Tag:
+# 		post_title = search_item
+# 		post_title_link = self.browser.find_element_by_link_text(post_title)
+# 		post_title_link.click()
+# 		time.sleep(1)
+
+# 		tag = self.browser.find_element_by_css_selector("p.pull-right").text
+# 		self.assertIn('Tag:', tag)
 
 
 
@@ -85,5 +119,6 @@ class NewVisitorTest(unittest.TestCase):
 
 		# self.fail('Finish the test.')
 
-if __name__ == '__main__':
-	unittest.main()
+# Following conditional not needed when using Django test runner
+# if __name__ == '__main__':
+# 	unittest.main()
